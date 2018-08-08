4b9459fde4d2b2f2342e01a77dc264afce622af4
Sarah Etter <sarah@territorial.ca>
2018-08-08 17:51:09 -0600

    Initial Commit

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
+          />
+          <link rel="stylesheet" href="/_next/static/style.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import "../styles.scss";
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>Sarah Etter Resume</title>
+    </Head>
+    <h1>Sarah Etter</h1>
+    <p>Resume contents will go here.</p>
+  </div>
+);
+
+export default Index;
