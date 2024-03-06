ea940b07e23d1b30ffb72521fa2f02419227962a
Sarah Etter <sarah@sarahetter.com>
2024-03-06 18:00:40 -0500

    switching to just html instead of next for no damn reason

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from "next/app"
-import "../styles/home.scss"
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
